a9158f3fab6330c962bd645c444053dd7f4ca6a9
Ori Livni <ori@orisomething.com>
2017-03-18 11:21:31 +0200

    Fix for GH pages for service workers

--- src/utilities/installServiceWorker.js
+++ src/utilities/installServiceWorker.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/sw.js").catch(function(err) {
+    navigator.serviceWorker.register("sw.js").catch(function(err) {
       // eslint-disable-next-line no-console
       console.log("ServiceWorker registration failed: ", err);
     });
